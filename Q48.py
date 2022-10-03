fc19120e0939db7e548cd7684f1cde7a55eb2516
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2022-10-03 01:08:08 -0400

    Update Q48.py

--- Q48.py
+++ Q48.py
@@ -1,8 +1,5 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
         for r in range(len(matrix)):
             for c in range(r):
                 matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
